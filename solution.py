1064106cb1740ea1a13a2f28af585a91e2a70aa0
Flower bloom <flower-blossom>
2022-07-10 20:50:46 +0700

    solution format

--- solution.py
+++ solution.py
@@ -1,15 +1,33 @@
+from turtle import distance
+
+
+def findMin(solution, distanceMatrix, p, idx):
+    min = float('inf')
+    for city in range(p):
+        distance = distanceMatrix[solution[city], idx]
+        if min > distance:
+            min = distance
+    return min
+
+
+def getTotalDistance(solutionList, distanceMatrix, p):
+    distance = 0
+    for idx in range(p + 1, len(solutionList)):
+        distance += findMin(solutionList, distanceMatrix, p, idx)
+    return distance
+
+
 class Solution:
     """
     Contains a list of customers and distanceMatrix
     """
-    def __init__(self, solutionMatrix, distanceMatrix):
-        self.solutionMatrix = solutionMatrix
+
+    def __init__(self, solutionList, distanceMatrix, p):
+        self.p = p
+        self.solutionList = solutionList
         self.distanceMatrix = distanceMatrix
-        self.totalDistance = self.__getTotalDistance()
+        self.totalDistance = getTotalDistance(
+            self.solutionList, self.distanceMatrix, self.p)
 
-    def __getTotalDistance(self):
-        distance = 0
-        for i, j in zip(self.customerList, self.customerList[1:] + [self.customerList[0]]):
-            distance += self.distanceMatrix[i, j]
-        return distance
-
+    def __copy__(self):
+        return self.__class__(self.solutionList.copy(), self.distanceMatrix, self.p)
